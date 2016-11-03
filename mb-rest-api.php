6a44660821b03927cacc665aec0015397ece5e64
Anh <anh@fitwp.com>
2016-11-03 14:01:52 +0700

    Reformat code and better check for MB Term Meta extension is installed

--- mb-rest-api.php
+++ mb-rest-api.php
@@ -16,108 +16,99 @@
  * @package    Meta Box
  * @subpackage MB Rest API
  */
-class MB_Rest_API
-{
-	/**
-	 * Constructor.
-	 */
-	public function __construct()
-	{
-		add_action( 'rest_api_init', array( $this, 'init' ) );
-	}
-
+class MB_Rest_API {
 	/**
 	 * Register new field 'meta_box' for all meta box's fields.
 	 */
-	public function init()
-	{
-		register_rest_field( $this->get_types(), 'meta_box', array( 'get_callback' => array( $this, 'get_post_meta' ) ) );
-		register_rest_field( $this->get_types( 'taxonomy' ), 'meta_box', array( 'get_callback' => array( $this, 'get_term_meta' ) ) );
+	public function init() {
+		register_rest_field( $this->get_types(), 'meta_box', array(
+			'get_callback' => array( $this, 'get_post_meta' ),
+		) );
+		register_rest_field( $this->get_types( 'taxonomy' ), 'meta_box', array(
+			'get_callback' => array( $this, 'get_term_meta' ),
+		) );
 	}
 
 	/**
 	 * Get post meta for the rest API.
-	 * @param array $object Post object
+	 *
+	 * @param array $object Post object.
+	 *
 	 * @return array
 	 */
-	public function get_post_meta( $object )
-	{
+	public function get_post_meta( $object ) {
 		$output     = array();
 		$meta_boxes = RWMB_Core::get_meta_boxes();
-		foreach ( $meta_boxes as $meta_box )
-		{
+		foreach ( $meta_boxes as $meta_box ) {
 			$meta_box = RW_Meta_Box::normalize( $meta_box );
-			if ( ! in_array( $object['type'], $meta_box['post_types'] ) )
-			{
+			if ( ! in_array( $object['type'], $meta_box['post_types'] ) ) {
 				continue;
 			}
-			foreach ( $meta_box['fields'] as $field )
-			{
-				if ( empty( $field['id'] ) )
-				{
+			foreach ( $meta_box['fields'] as $field ) {
+				if ( empty( $field['id'] ) ) {
 					continue;
 				}
-				$output[$field['id']] = rwmb_get_value( $field['id'] );
+				$output[ $field['id'] ] = rwmb_get_value( $field['id'] );
 			}
 		}
+
 		return $output;
 	}
 
 	/**
 	 * Get term meta for the rest API.
+	 *
 	 * @param array $object Term object
+	 *
 	 * @return array
 	 */
-	public function get_term_meta( $object )
-	{
-		global $mb_term_meta_boxes;
-		RWMB_Core::get_meta_boxes();
+	public function get_term_meta( $object ) {
 		$output = array();
-
-		if( !is_array( $mb_term_meta_boxes ) || !$mb_term_meta_boxes instanceof Traversable )
+		if ( ! class_exists( 'MB_Term_Meta_Box' ) ) {
 			return $output;
+		}
 
-		foreach ( $mb_term_meta_boxes as $meta_box )
-		{
-			if ( ! in_array( $object['taxonomy'], (array) $meta_box['taxonomies'] ) )
-			{
+		RWMB_Core::get_meta_boxes();
+		$meta_boxes = MB_Term_Meta_Loader::$meta_boxes;
+
+		foreach ( $meta_boxes as $meta_box ) {
+			if ( ! in_array( $object['taxonomy'], (array) $meta_box['taxonomies'] ) ) {
 				continue;
 			}
 			$fields = RW_Meta_Box::normalize_fields( $meta_box['fields'] );
-			foreach ( $fields as $field )
-			{
-				if ( empty( $field['id'] ) )
-				{
+			foreach ( $fields as $field ) {
+				if ( empty( $field['id'] ) ) {
 					continue;
 				}
-				$single               = $field['clone'] || ! $field['multiple'];
-				$output[$field['id']] = get_term_meta( $object['id'], $field['id'], $single );
+				$single                 = $field['clone'] || ! $field['multiple'];
+				$output[ $field['id'] ] = get_term_meta( $object['id'], $field['id'], $single );
 			}
 		}
+
 		return $output;
 	}
 
 	/**
 	 * Get supported types in Rest API.
+	 *
 	 * @param string $type 'post' or 'taxonomy'
+	 *
 	 * @return array
 	 */
-	protected function get_types( $type = 'post' )
-	{
+	protected function get_types( $type = 'post' ) {
 		$types = get_post_types( array(), 'objects' );
-		if ( 'taxonomy' == $type )
-		{
+		if ( 'taxonomy' == $type ) {
 			$types = get_taxonomies( array(), 'objects' );
 		}
-		foreach ( $types as $type => $object )
-		{
-			if ( empty( $object->show_in_rest ) )
-			{
-				unset( $types[$type] );
+		foreach ( $types as $type => $object ) {
+			if ( empty( $object->show_in_rest ) ) {
+				unset( $types[ $type ] );
 			}
 		}
+
 		return array_keys( $types );
 	}
 }
 
-new MB_Rest_API;
+$mb_rest_api = new MB_Rest_API;
+add_action( 'rest_api_init', array( $mb_rest_api, 'init' ) );
