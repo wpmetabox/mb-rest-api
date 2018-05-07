275e00e5bf5d63b786c72b4afabfb7d7fa7a0a69
Truong Giang <truongwp@gmail.com>
2018-05-07 08:25:15 +0000

    Add term updating and user support

--- class-mb-rest-api.php
+++ class-mb-rest-api.php
@@ -20,6 +20,11 @@ class MB_Rest_API {
 		) );
 		register_rest_field( $this->get_types( 'taxonomy' ), 'meta_box', array(
 			'get_callback' => array( $this, 'get_term_meta' ),
+			'update_callback' => array( $this, 'update_term_meta' ),
+		) );
+		register_rest_field( 'user', 'meta_box', array(
+			'get_callback' => array( $this, 'get_user_meta' ),
+			'update_callback' => array( $this, 'update_user_meta' ),
 		) );
 	}
 
@@ -130,6 +135,115 @@ class MB_Rest_API {
 		}
 
 		return $output;
+	}
+
+	/**
+	 * Update term meta for the rest API.
+	 *
+	 * @param string|array $data   Term meta values in either JSON or array format.
+	 * @param object       $object Term object.
+	 */
+	public function update_term_meta( $data, $object ) {
+		if ( is_string( $data ) ) {
+			$data = json_decode( $data, true );
+			if ( JSON_ERROR_NONE === json_last_error() ) {
+				return;
+			}
+		}
+
+		foreach ( $data as $field_id => $value ) {
+			$field = rwmb_get_registry( 'field' )->get( $field_id, $object->taxonomy );
+			$old   = RWMB_Field::call( $field, 'raw_meta', $object->term_id );
+			$new   = $value;
+
+			// Allow field class change the value.
+			if ( $field['clone'] ) {
+				$new = RWMB_Clone::value( $new, $old, $object->term_id, $field );
+			} else {
+				$new = RWMB_Field::call( $field, 'value', $new, $old, $object->term_id );
+				$new = RWMB_Field::filter( 'sanitize', $new, $field );
+			}
+			$new = RWMB_Field::filter( 'value', $new, $field, $old );
+			$new = RWMB_Field::filter( 'rest_value', $new, $field, $old, $object->term_id );
+
+			// Call defined method to save meta value, if there's no methods, call common one.
+			RWMB_Field::call( $field, 'save', $new, $old, $object->term_id );
+		}
+	}
+
+	/**
+	 * Get user meta for the rest API.
+	 *
+	 * @param array $object User object.
+	 *
+	 * @return array
+	 */
+	public function get_user_meta( $object ) {
+		$output = array();
+		if ( ! class_exists( 'MB_User_Meta_Box' ) ) {
+			return $output;
+		}
+
+		$meta_boxes = MB_User_Meta_Box::$meta_boxes;
+
+		foreach ( $meta_boxes as $meta_box ) {
+			if ( empty( $meta_box->type ) || 'user' !== $meta_box->type ) {
+				continue;
+			}
+			$fields = RW_Meta_Box::normalize_fields( $meta_box['fields'] );
+			foreach ( $fields as $field ) {
+				if ( empty( $field['id'] ) ) {
+					continue;
+				}
+				$single      = $field['clone'] || ! $field['multiple'];
+				$field_value = get_user_meta( $object['id'], $field['id'], $single );
+
+				/*
+				 * Make sure values of file/image fields are always indexed 0, 1, 2, ...
+				 * @link https://github.com/malfborger/mb-rest-api/commit/31aa8fa445c188e8a71ebff80027acbcaa0fd268
+				 */
+				if ( is_array( $field_value ) && in_array( $field['type'], array( 'media', 'file', 'file_upload', 'file_advanced', 'image', 'image_upload', 'image_advanced', 'plupload_image', 'thickbox_image' ), true ) ) {
+					$field_value = array_values( $field_value );
+				}
+				$output[ $field['id'] ] = $field_value;
+			}
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Update user meta for the rest API.
+	 *
+	 * @param string|array $data   User meta values in either JSON or array format.
+	 * @param object       $object User object.
+	 */
+	public function update_user_meta( $data, $object ) {
+		if ( is_string( $data ) ) {
+			$data = json_decode( $data, true );
+			if ( JSON_ERROR_NONE === json_last_error() ) {
+				return;
+			}
+		}
+
+		foreach ( $data as $field_id => $value ) {
+			$field = rwmb_get_registry( 'field' )->get( $field_id, 'user' );
+			$old   = RWMB_Field::call( $field, 'raw_meta', $object->ID );
+			$new   = $value;
+
+			// Allow field class change the value.
+			if ( $field['clone'] ) {
+				$new = RWMB_Clone::value( $new, $old, $object->ID, $field );
+			} else {
+				$new = RWMB_Field::call( $field, 'value', $new, $old, $object->ID );
+				$new = RWMB_Field::filter( 'sanitize', $new, $field );
+			}
+			$new = RWMB_Field::filter( 'value', $new, $field, $old );
+			$new = RWMB_Field::filter( 'rest_value', $new, $field, $old, $object->ID );
+
+			// Call defined method to save meta value, if there's no methods, call common one.
+			RWMB_Field::call( $field, 'save', $new, $old, $object->ID );
+		}
 	}
 
 	/**
